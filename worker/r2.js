7cc12d59bc94435f5ea9c6610d1b24924420cdd3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-29 14:02:19 +0100

    rename keyRaw to encodedKey

--- worker/r2.js
+++ worker/r2.js
@@ -7,8 +7,8 @@
  */
 export const r2Handle = async (R2, OPERATION, req) => {
   const url = new URL(req.url);
-  const [, , , keyRaw] = url.pathname.split("/");
-  const key = decodeURIComponent(keyRaw);
+  const [, , , encodedKey] = url.pathname.split("/");
+  const key = decodeURIComponent(encodedKey);
   const options = JSON.parse(req.headers.get("CF-R2-OPTIONS") ?? "{}");
 
   if (OPERATION === "r2_head") {
