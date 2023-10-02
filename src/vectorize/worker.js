b2c3c684c2b53839978455e738db61b96bc206dd
Yuji Sugiura <y.sugiura.0316@gmail.com>
2023-10-02 14:01:35 +0900

    Fix case

--- src/vectorize/worker.js
+++ src/vectorize/worker.js
@@ -9,49 +9,49 @@ export const isVectorizeBinding = (binding) =>
   binding.constructor.name === "VectorizeIndexImpl";
 
 /**
- * @param {VectorizeIndex} Vectorize
+ * @param {VectorizeIndex} VECTORIZE
  * @param {Request} req
  */
-export const handleVectorizeDispatch = async (Vectorize, req) => {
+export const handleVectorizeDispatch = async (VECTORIZE, req) => {
   const { operation, parameters } = await req.text().then((t) => parse(t));
 
   if (operation === "VectorizeIndex.describe") {
-    const result = await Vectorize.describe();
+    const result = await VECTORIZE.describe();
 
     return Response.json(result);
   }
 
   if (operation === "VectorizeIndex.query") {
     const [vector, options] = parameters;
-    const result = await Vectorize.query(vector, options);
+    const result = await VECTORIZE.query(vector, options);
 
     return Response.json(result);
   }
 
   if (operation === "VectorizeIndex.insert") {
     const [vectors] = parameters;
-    const result = await Vectorize.insert(vectors);
+    const result = await VECTORIZE.insert(vectors);
 
     return Response.json(result);
   }
   
   if (operation === "VectorizeIndex.upsert") {
     const [vectors] = parameters;
-    const result = await Vectorize.upsert(vectors);
+    const result = await VECTORIZE.upsert(vectors);
 
     return Response.json(result);
   }
 
   if (operation === "VectorizeIndex.deleteByIds") {
     const [ids] = parameters;
-    const result = await Vectorize.deleteByIds(ids);
+    const result = await VECTORIZE.deleteByIds(ids);
 
     return Response.json(result);
   }
 
   if (operation === "VectorizeIndex.getByIds") {
     const [ids] = parameters;
-    const result = await Vectorize.getByIds(ids);
+    const result = await VECTORIZE.getByIds(ids);
 
     return Response.json(result);
   }
